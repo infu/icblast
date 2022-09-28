5886108cb89531eccf1ce5abb921ec376f56ead7
infu <vtsukev@aula.bg>
2022-09-28 13:43:16 +0300

    fixed ii

--- src/sys/internetidentity.js
+++ src/sys/internetidentity.js
@@ -1,7 +1,11 @@
 import express from "express";
 import serve from "express-static";
 import open from "open";
-import dfidentity from "@dfinity/identity";
+import {
+  Ed25519KeyIdentity,
+  DelegationChain,
+  DelegationIdentity,
+} from "@dfinity/identity";
 import * as url from "url";
 import path from "path";
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
@@ -10,23 +14,12 @@ export const internetIdentity = () => {
   return new Promise(async (resolve, reject) => {
     const app = express();
 
-    app.use(function (req, res, next) {
-      var data = "";
-      req.setEncoding("binary");
-      req.on("data", function (chunk) {
-        data += chunk;
-      });
-
-      req.on("end", function () {
-        req.body = data;
-        next();
-      });
-    });
+    app.use(express.json());
 
     app.post("/key", function (req, res) {
-      let buff = new Buffer.from(req.body, "binary");
-      let rawBuffer = new Uint8Array(buff).buffer;
-      let identity = dfidentity.Ed25519KeyIdentity.fromSecretKey(rawBuffer);
+      let key = Ed25519KeyIdentity.fromJSON(req.body.key);
+      let chain = DelegationChain.fromJSON(req.body.chain);
+      let identity = DelegationIdentity.fromDelegation(key, chain);
       resolve(identity);
       res.send("");
       server.close();
