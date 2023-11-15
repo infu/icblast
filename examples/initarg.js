f174317960e925494f4c6a68a36cd70b5c1f1070
infu <vtsukev@aula.bg>
2023-11-15 18:46:19 +0200

    example idl convert

---
+++ examples/initarg.js
@@ -0,0 +1,30 @@
+import { explainer, convert } from "../src/index.js";
+import { IDL } from "@dfinity/candid";
+import { init, idlFactory } from "./ledger.idl.js";
+
+let ledger_args = {
+  Init: {
+    minting_account: {
+      owner: "aaaaa-aa",
+    },
+    fee_collector_account: { owner: "aaaaa-aa" },
+    transfer_fee: 10000,
+    decimals: 8,
+    token_symbol: "Test",
+    token_name: "Test",
+    metadata: [],
+    initial_balances: [[{ owner: "aaaaa-aa" }, 100000000000]],
+    archive_options: {
+      num_blocks_to_archive: 10000,
+      trigger_threshold: 9000,
+      controller_id: "aaaaa-aa",
+    },
+  },
+};
+
+const xdl = explainer(init);
+const processedArgs = convert([ledger_args], xdl);
+const interm = init({ IDL });
+let result = IDL.encode(interm, processedArgs);
+console.log(result);
+console.log(processedArgs);
